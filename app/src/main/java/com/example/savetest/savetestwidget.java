307a0cc1f98816701590a0e0fea4bcb438ee5524
Dorian <oniman2013@gmail.com>
2019-04-23 14:35:49 -0700

    widget works

--- app/src/main/java/com/example/savetest/savetestwidget.java
+++ app/src/main/java/com/example/savetest/savetestwidget.java
@@ -1,9 +1,14 @@
 package com.example.savetest;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +18,10 @@ public class savetestwidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.savetestwidget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -27,6 +32,31 @@ public class savetestwidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+
+        for(int j = 0; j < appWidgetIds.length; j++)
+        {
+            int appWidgetId = appWidgetIds[j];
+
+            try {
+                Intent intent = new Intent("android.intent.action.MAIN");
+                intent.addCategory("android.intent.category.LAUNCHER");
+
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.setComponent(new ComponentName(context.getPackageName(),
+                        secondact.class.getName()));
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        context, 0, intent, 0);
+                RemoteViews views = new RemoteViews(context.getPackageName(),
+                        R.layout.savetestwidget);
+                views.setOnClickPendingIntent(R.id.widbut, pendingIntent);
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(context.getApplicationContext(),
+                        "There was a problem loading the application: ",
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
